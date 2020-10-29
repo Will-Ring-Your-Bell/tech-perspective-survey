203cbcb2e4873c0f301b862038193fa41c3c9a56
Mark Bell <skrambelled@gmail.com>
2020-10-29 14:47:16 -0700

    schema update'

--- data/schema.sql
+++ data/schema.sql
@@ -1,28 +1,7 @@
-DROP TABLE IF EXISTS forms;
+DROP TABLE IF EXISTS surveys;
 
-CREATE TABLE forms(
-  instanceID SERIAL PRIMARY KEY,
-  adminID NUMERIC,
-  title VARCHAR(255),
-  jotformId NUMERIC,
-  timestamp NUMERIC,
-  totalPeople NUMERIC,
-  totalQuestions NUMERIC,
-  closed BOOLEAN
-);
-
-DROP TABLE IF EXISTS people;
-
-CREATE TABLE people(
-  peopleID SERIAL PRIMARY KEY,
-  instanceID NUMERIC,
-  numTrue NUMERIC
-);
-
-DROP TABLE IF EXISTS admin;
-
-CREATE TABLE admin(
-  adminID SERIAL PRIMARY KEY,
-  username VARCHAR(255),
-  apiKey VARCHAR(255)
-);
+CREATE TABLE surveys(
+  id SERIAL PRIMARY KEY,
+  surveyid VARCHAR(255),
+  results VARCHAR(255)
+)
